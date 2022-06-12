113e6fc27176dd5d82ed445cb5c90cc5fa134612
alylopez02 <alylopez0217@gmail.com>
2022-06-12 14:19:21 -0400

    started inquirer prompt

--- index.js
+++ index.js
@@ -1,13 +1,90 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your full name?",
+    "What is the title of your project?",
+    "Please write a description of the project.",
+    "Please input the installation instructions.",
+    "Who contributed in this project?",
+    "Please input test instructions here.",
+    "Please choose the license that your application is covered under from the following.",
+    "What is your GitHub username?",
+    "What is your email address?",
+    "How should you be reached if someone has additional questions?"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+//     fs.writeFile("project.md", md);
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: questions[0]
+    },
+    {
+        type: "input",
+        name: "title",
+        message: questions[1]
+    },
+    {
+        type: "input",
+        name: "description",
+        message: questions[2]
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: questions[3]
+    },
+    {
+        type: "input",
+        name: "contributers",
+        message: questions[4]
+    },
+    {
+        type: "input",
+        name: "test",
+        message: questions[5]
+    },
+    {
+        type: "list",
+        name: "license",
+        message: questions[6],
+        // choices: ["", "", "", "", "", ""] // input choices
+    },
+    {
+        type: "input",
+        name: "username",
+        message: questions[7]
+    },
+    {
+        type: "input",
+        name: "email",
+        message: questions[8]
+    },
+    {
+        type: "input",
+        name: "reach",
+        message: questions[9]
+    }
+
+])
+.then(
+
+)
+
+}
+
+
+
 
 // Function call to initialize app
 init();
